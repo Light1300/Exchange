6a0089b4024e77efb27f42bba427536a9d4c68d4
Sarvesh Patil <sarupatil0001@gmail.com>
2026-01-29 22:56:39 +0530

    final copy with right folder placement

--- exchange-frontend/app/utils/httpClient.ts
+++ exchange-frontend/app/utils/httpClient.ts
@@ -29,3 +29,15 @@ export async function getTrades(market:string): Promise<Trade[]>{
     const response = await axios.get(`${BASE_URL}/trades?symbol=${market}`)
     return response.data;
 }
+
+
+export async function getKlines( market: string, interval: string, startTines: number, endTime: number):Promise<KLine[]> {
+    const response = await axios.get(`${BASE_URL}/klines?symbol=${market}&interval=${interval}&startTime=${startTines}&endTime=${endTime}`);
+    const data: KLine[]= response.data;
+    return data.sort((x,y) => (Number(x.end) < Number(y.end)? -1:1));
+}
+
+export async function getMarkets(): Promise<string[]> {
+    const response = await axios.get(`${BASE_URL}/markets`);
+    return response.data;
+}
