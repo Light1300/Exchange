8168b1b85ddc037d4b271cf2372189be93a42f69
Light1300 <sarupatil0001@gmail.com>
2026-01-31 21:37:45 +0530

    resolved conflicts

--- exchange-frontend/app/layout.tsx
+++ exchange-frontend/app/layout.tsx
@@ -1,9 +1,8 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { Appbar } from "./components/Appbar";
 
+<<<<<<< Updated upstream
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -11,18 +10,24 @@ export const metadata: Metadata = {
   description: "My Exchange App",
 };
 
+=======
+>>>>>>> Stashed changes
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
+<<<<<<< Updated upstream
       <body
         className={inter.className}>
           <Appbar />
         {children}
       </body>
+=======
+      <body>{children}</body>
+>>>>>>> Stashed changes
     </html>
   );
 }
--- exchange-frontend/app/page.tsx
+++ exchange-frontend/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
+<<<<<<< Updated upstream
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Landing page
       </main>
+=======
+    <main className="min-h-screen bg-blue-600 text-white flex items-center justify-center">
+      Tailwind is working 🚀
+    </main>
+>>>>>>> Stashed changes
   );
 }
