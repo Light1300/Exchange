5c6786906f9e84c6b768ce8901ed8d36978401a0
Light1300 <sarupatil0001@gmail.com>
2026-01-31 22:00:38 +0530

    Update UI and trade pages

--- exchange-frontend/app/trade/[market]/page.tsx
+++ exchange-frontend/app/trade/[market]/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { MarketBar } from "@/app/components/MarketBar";
-import { SwapUi } from "@/app/components/SwapUi";
+import { SwapUI } from "@/app/components/SwapUI";
 import { TradeView } from "@/app/components/TradeView";
-import { Depth } from "@/app/components/Depth";
+import { Depth } from "@/app/components/depth/Depth";
 import { useParams } from "next/navigation";
 
 export default function Page() {
@@ -21,7 +21,7 @@ export default function Page() {
             <div className="w-[1px] flex-col border-slate-800 border-l"></div>
             <div >
                 <div className="flex flex-col w-[250px]">
-                    <SwapUi market={market as string} />
+                    <SwapUI market={market as string} />
                 </div>
             </div>
         </div>       
