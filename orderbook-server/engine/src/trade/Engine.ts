ade7ed9807f8c65651024585e0dcf6199d3a8431
Light1300 <sarupatil0001@gmail.com>
2026-02-03 00:17:09 +0530

    engine pushed again

--- orderbook-server/engine/src/trade/Engine.ts
+++ orderbook-server/engine/src/trade/Engine.ts
@@ -18,9 +18,40 @@ export class Engine {
     constructor(){
         let snapshot = null;
         try{
-
+            if(process.env.WITH_SNAPSHOT){
+                snapshot = fs.readFileSync("./snapshot.json");
+            }
         } catch(error){
             console.log("No snapshot Found ::: ",error)
+        }
+
+        if(snapshot){
+            const snapshotSnapshot = JSON.parse(snapshot.toString());
+            this.orderbooks = snapshotSnapshot.orderbooks.map((o:any) => new Orderbook(o.baseAsset, o.bids, o.asks, o.lastTradeId, o.currentPrice  ));
+            this.balances = new Map(snapshotSnapshot.balances);
+        } else{
+            this.orderbooks = [new Orderbook(`TATA`, [], [], 0,0)];
+            this.setBaseBalances();
+        }
+        setInterval(()=>{
+            this.saveSnapshot();
+        }, 1000*3);
+
+
+        saveSnapshot(){
+            const snapshotSnapshot = {
+                orderbooks: this.orderbooks.map(o=> o.getSnapshot()),
+                balances: Array.from(this.balances.entries())
+            }
+            fs.writeFileSync("./snapshot.json", JSON.stringify(snapshotSnapshot));
+        }
+        process({message,clientId}: {message:MessageFromApi, clientId:string}){
+            switch(message.type){
+                case CANCLE_ORDER:
+                    try{
+                    const { executedQty, fills, orderId } = this.createOrder(message.data.market, message.data.price, message.data.quantity, message.data.side, message.data.userId);
+                    }   
+            }
         }
     }
 }
