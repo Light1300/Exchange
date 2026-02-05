8e7cb77b576d187631d49897ed176102c5413de6
Sarvesh Patil <sarupatil0001@gmail.com>
2026-02-05 15:05:09 +0530

    Add files via upload

---
+++ orderbook-server/mm/src/index.ts
@@ -0,0 +1,86 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:3000";
+const TOTAL_BIDS = 15;
+const TOTAL_ASK = 15;
+const MARKET = "TATA_INR";
+const USER_ID = "5";
+
+async function main(){
+    const price = 1000 + Math.random() + 10;
+    const openOrders = await axios.get(`${BASE_URL}/api/v1/order/open?=userId=${USER_ID}&market=${MARKET}`);
+
+    const totalBids = openOrders.data.filter((o: any ) => o.side === "buy").length;
+    const totalAsks = openOrders.data.filter((o: any)=> o.side ==="sell").length;
+
+    const canclledBids = await cancleBidMoreThan(openOrders.data, price);
+    const cancleAsks = await cancleAsksLessThan(openOrders.data, price);
+
+    let bidsToAdd = TOTAL_ASK - totalBids - cancleAsks;
+    let asksToAdd = TOTAL_ASK - totalAsks - cancleAsks;
+
+    while(bidsToAdd > 0 || asksToAdd >0){
+        if(bidsToAdd> 0){
+            await axios.post(`${BASE_URL}/api/v1/order`, {
+                market: MARKET,
+                price: (price - Math.random() *1).toFixed(1).toString(),
+                quantity:"1",
+                side:"buy",
+                userId: USER_ID,
+                });
+                bidsToAdd--;
+        }
+        if(asksToAdd>0){
+            await axios.post(`${BASE_URL}/api/v1/order`, {
+                market: MARKET,
+                price: (price + Math.random() *1).toFixed(1).toString(),
+                quantity: "1",
+                side:"sell",
+                userId: USER_ID,
+            });
+            asksToAdd--;
+        }
+    }
+        await new Promise(resolve => setTimeout(resolve,1000));
+
+        main();
+}
+
+
+async function cancleBidsMoreThan(openOrders:any[], price:number){
+    let promises: any[] = [];
+    openOrders.map( o =>{
+        if(o.side === "buy" && (o.price > price) || Math.random() < 0.1){
+            promises.push(axios.delete(`${BASE_URL}/api/v1/order`, {
+                data: {
+                    orderId: o.orderId,
+                    market: MARKET,
+                }
+            }));
+        }
+    });
+
+    await Promise.all(promises);
+    return promises.length;
+}
+
+
+async function cancelAsksLessThan(openOrders: any[], price: number) {
+    let promises: any[] = [];
+    openOrders.map(o => {
+        if (o.side === "sell" && (o.price < price || Math.random() < 0.5)) {
+            promises.push(axios.delete(`${BASE_URL}/api/v1/order`, {
+                data: {
+                    orderId: o.orderId,
+                    market: MARKET
+                }
+            }));
+        }
+    });
+    await Promise.all([promises]);
+    return promises.length
+    
+}
+
+
+main();
