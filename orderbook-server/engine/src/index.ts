3c6b16c633f314649673d16acc369bef254023ba
Sarvesh Patil <sarupatil0001@gmail.com>
2026-02-05 14:35:18 +0530

    index for engine

---
+++ orderbook-server/engine/src/index.ts
@@ -0,0 +1,21 @@
+import { createClient } from  "redis";
+import { Engine } from "./trade/Engine.js";
+
+async function main(){
+    const engine = new Engine();
+    const redisClient = createClient();
+    await redisClient.connect();
+    console.log("redis is connectd");
+
+    while(true){
+        const response = await redisClient.rPop("message" as string);
+        if(!response{
+            
+        }else{
+            engine.process(JSON.parse(response));
+        }
+    }
+}
+
+
+main();
